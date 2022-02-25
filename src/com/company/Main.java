bc79f6a876dd8416c419e15ef454c441c00a4df1
bjoexxxx <bjoe6702@stud.kea.dk>
2022-02-25 13:20:32 +0100

    Added askAboutAge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,24 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
 	// write your code here
         Main action = new Main();
         action.Greet();
+        action.askAboutAge();
         System.out.println("it works!");
     }
     public void Greet(){
-        Scanner keyboard = new Scanner(System.in);
         System.out.println("intast navn");
         String navn = keyboard.nextLine();
         System.out.println(navn);
         System.out.println("hej "+navn);
+    }
+    public void askAboutAge(){
+        System.out.println("indtast alder");
+        int alder = keyboard.nextInt();
+        System.out.println(alder);
     }
 }
