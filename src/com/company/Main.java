9d8402c52df644210c1bc6547bef2754a9b50fd4
bjoexxxx <bjoe6702@stud.kea.dk>
2022-02-25 13:15:11 +0100

    Added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Main action = new Main();
+        action.Greet();
         System.out.println("it works!");
+    }
+    public void Greet(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("intast navn");
+        String navn = keyboard.nextLine();
+        System.out.println(navn);
+        System.out.println("hej "+navn);
     }
 }
